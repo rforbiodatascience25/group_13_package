2bd7b99799944439f6eecb6966845346e1f6215d
MikelGarciaData <mikelgarciadata@gmail.com>
2025-10-28 10:08:52 +0100

    uploading plot_base_freq (function 5) and traduction_prot (function 4)

--- R/plot_base_freq.R
+++ R/plot_base_freq.R
@@ -1,20 +1,20 @@
 plot_base_seq <- function(seq){
-  name_me3 <- seq |>  
+  base_list <- seq |>  
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
     as.character() |> 
     unique()
   
-  counts <- sapply(name_me3, function(amino_acid) stringr::str_count(string = name_me2, pattern =  amino_acid)) |>  
+  counts <- sapply(base_list, function(amino_acid) stringr::str_count(string = seq, pattern =  amino_acid)) |>  
     as.data.frame()
   
   colnames(counts) <- c("Counts")
-  counts[["Name_me2"]] <- rownames(counts)
+  counts[["Nucleotides"]] <- rownames(counts)
   
-  name_me4 <- counts |>  
-    ggplot2::ggplot(ggplot2::aes(x = Name_me2, y = Counts, fill = Name_me2)) +
+  bar_plot <- counts |>  
+    ggplot2::ggplot(ggplot2::aes(x = Nucleotides, y = Counts, fill = Nucleotides)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw() +
     ggplot2::theme(legend.position = "none")
   
-  return(name_me4)
+  return(bar_plot)
 }
