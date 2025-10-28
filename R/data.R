ee59c0ea75c9cafbe7cacc44e3ebcf61fcec4ec7
joshinsky <joshinsky@outlook.de>
2025-10-28 09:54:01 +0100

    Create genetic_code object

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Genetic Code Data
+#'
+#' This is the genetic code for AA from DNA sequence
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"genetic_code"
